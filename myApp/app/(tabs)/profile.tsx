fa074b054b66b385f69213dcb199fa5155d99a72
erasheqiri1 <154844824+erasheqiri1@users.noreply.github.com>
2025-10-13 14:28:18 +0300

    testim profile.tsx

--- myApp/app/(tabs)/profile.tsx
+++ myApp/app/(tabs)/profile.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
-export default function Dashboard() {
+export default function profile() {
   return (
     <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
-      <Text style={{ fontSize:18, fontWeight:'600' }}>Dashbo55555555555
-        
+      <Text style={{ fontSize:18, fontWeight:'600' }}>profile per testim
+
       </Text>
     </View>
   );
