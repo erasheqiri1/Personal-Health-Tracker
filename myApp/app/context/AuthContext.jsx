fac2d1b027103ce735ea67514b02e40b83e4e89a
auroresmirqaku <95504058+auroresmirqaku@users.noreply.github.com>
2025-11-14 16:24:25 +0100

    create AuthContext.jsx

--- myApp/app/context/AuthContext.jsx
+++ myApp/app/context/AuthContext.jsx
@@ -1,1 +1,30 @@
-x
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../../firebaseConfig"; // sigurohu që emri është i saktë
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (u) => {
+      setUser(u || null);
+      setLoading(false);
+    });
+    return unsub;
+  }, []);
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);x
