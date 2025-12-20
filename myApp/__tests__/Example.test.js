901b4471dde48319f0c927ea885127aa63ec9628
ArtinDulahi <116291477+ArtinDulahi@users.noreply.github.com>
2025-12-20 15:04:46 +0100

    Add files via upload

---
+++ myApp/__tests__/Example.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { Text, View } from 'react-native';
+
+// A tiny component for testing
+const SimpleComponent = () => (
+    <View>
+        <Text>Hello Health Tracker!</Text>
+    </View>
+);
+
+describe('Smoke Test', () => {
+    it('renders correctly', () => {
+        const { getByText } = render(<SimpleComponent />);
+        expect(getByText('Hello Health Tracker!')).toBeTruthy();
+    });
+});
