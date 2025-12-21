8fa1fd9bdbc28e40113c1ca01efd02cdf77b8c0a
erasheqiri1 <154844824+erasheqiri1@users.noreply.github.com>
2025-12-22 01:11:55 +0200

    U
    Removed commented sections for better readability.

--- myApp/app/_layout.jsx
+++ myApp/app/_layout.jsx
@@ -5,19 +5,18 @@ import React from 'react';
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* Grupi kryesor me tabat për përdoruesin normal */}
+ 
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-      {/* Grupi i auth (login, signup) */}
+   
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
 
-      {/* Planet e ushqimit (shto_pesh, humb_pesh, mbaj_pesh) */}
+    
       <Stack.Screen name="plans" options={{ headerShown: false }} />
 
-      {/* Ushtrimet për user (homeworkout, weightlifting) */}
       <Stack.Screen name="ushtrime" options={{ headerShown: false }} />
 
-      {/* Zona e adminit (admin/dashboard, admin/ushqime, admin/ushtrime, ...) */}
+  
       <Stack.Screen name="(admin)" options={{ headerShown: false }} />
     </Stack>
   );
